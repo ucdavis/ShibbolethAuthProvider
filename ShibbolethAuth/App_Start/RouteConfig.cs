3d4b947ea9c00aa682c4ef80a06da90b5d463a8f
Scott Kirkland <srkirkland@gmail.com>
2016-08-17 10:00:54 -0700

    add logout direct link

--- ShibbolethAuth/App_Start/RouteConfig.cs
+++ ShibbolethAuth/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace ShibbolethAuth
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Logout", "logout", new {controller = "Home", action = "Logout"});
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
