f218738545ddbef484a7bc8a2635d8e097b68f73
Scott Kirkland <srkirkland@gmail.com>
2016-08-19 10:02:42 -0700

    remove openid notification

--- ShibbolethAuth/Startup.cs
+++ ShibbolethAuth/Startup.cs
@@ -83,37 +83,37 @@ namespace ShibbolethAuth
 
                 Notifications = new OpenIdConnectAuthenticationNotifications
                 {
-                    SecurityTokenValidated = async n =>
-                    {
-                        var nid = new ClaimsIdentity(
-                            n.AuthenticationTicket.Identity.AuthenticationType,
-                            Constants.ClaimTypes.GivenName,
-                            Constants.ClaimTypes.Role);
+                    //SecurityTokenValidated = async n =>
+                    //{
+                    //    var nid = new ClaimsIdentity(
+                    //        n.AuthenticationTicket.Identity.AuthenticationType,
+                    //        Constants.ClaimTypes.GivenName,
+                    //        Constants.ClaimTypes.Role);
 
-                        // get userinfo data
-                        var userInfoClient = new UserInfoClient(
-                            new Uri(n.Options.Authority + "/connect/userinfo"),
-                            n.ProtocolMessage.AccessToken);
+                    //    // get userinfo data
+                    //    var userInfoClient = new UserInfoClient(
+                    //        new Uri(n.Options.Authority + "/connect/userinfo"),
+                    //        n.ProtocolMessage.AccessToken);
 
-                        var userInfo = await userInfoClient.GetAsync();
-                        userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
+                    //    var userInfo = await userInfoClient.GetAsync();
+                    //    userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
 
-                        // keep the id_token for logout
-                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+                    //    // keep the id_token for logout
+                    //    nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
 
-                        // add access token for sample API
-                        nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
+                    //    // add access token for sample API
+                    //    nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
 
-                        // keep track of access token expiration
-                        nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
+                    //    // keep track of access token expiration
+                    //    nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
 
-                        // add some other app specific claim
-                        nid.AddClaim(new Claim("app_specific", "some data"));
+                    //    // add some other app specific claim
+                    //    nid.AddClaim(new Claim("app_specific", "some data"));
 
-                        n.AuthenticationTicket = new AuthenticationTicket(
-                            nid,
-                            n.AuthenticationTicket.Properties);
-                    },
+                    //    n.AuthenticationTicket = new AuthenticationTicket(
+                    //        nid,
+                    //        n.AuthenticationTicket.Properties);
+                    //},
 
                     RedirectToIdentityProvider = n =>
                     {
