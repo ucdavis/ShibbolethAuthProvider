5ff117faf7aa05de781e617324215536f885cca0
Scott Kirkland <srkirkland@gmail.com>
2016-08-19 10:36:02 -0700

    transient nameid

--- ShibbolethAuth/Startup.cs
+++ ShibbolethAuth/Startup.cs
@@ -143,7 +143,7 @@ namespace ShibbolethAuth
                 {
                     EntityId = new EntityId(BaseUrl),
                     ReturnUrl = new Uri(BaseUrl),
-                    NameIdPolicy = new Saml2NameIdPolicy(true, NameIdFormat.Persistent),
+                    NameIdPolicy = new Saml2NameIdPolicy(false, NameIdFormat.Transient),
                     AttributeConsumingServices = { GetAttributeService() },
                     //AuthenticateRequestSigningBehavior = SigningBehavior.Never // TODO: decide what needs to be here in prod
                 },
